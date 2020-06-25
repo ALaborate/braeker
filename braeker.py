af3163d998b54229bd863cdf7620e62d8b0fe7a8
OMOH98 <gleb.kosiachenko@logika.com.ua>
2020-06-25 07:16:31 +0300

    MVP functionality

--- braeker.py
+++ braeker.py
@@ -1,4 +1,18 @@
 import winsound
+import time
+import datetime
+import sys
+period = None
+start = None
+if __name__ == '__main__':
 
-if __name__=='__main__':
-    winsound.PlaySound('brokenGlass.wav', winsound.SND_FILENAME)
+    while True:
+        if not start:
+            period = int(
+                input(f'[{datetime.datetime.now()}] Enter work period in seconds ->'))
+            start = datetime.datetime.now()
+        elif (datetime.datetime.now() - start).total_seconds() > period:
+            start = None
+            winsound.PlaySound('brokenGlass.wav',
+                               winsound.SND_FILENAME | winsound.SND_ASYNC)
+        else: time.sleep(period)
