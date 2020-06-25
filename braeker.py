3fdd39fc110744d6d9af66da9f64ce056d33bf7d
OMOH98 <gleb.kosiachenko@logika.com.ua>
2020-06-25 09:27:22 +0300

    Featuring: minimizing and suffixes

--- braeker.py
+++ braeker.py
@@ -1,23 +1,40 @@
 import winsound
 import time
 import datetime
+import re
 import ctypes
 from ctypes import wintypes
 
 user32 = ctypes.windll.user32
-SW_SHOWMAXIMIZED = 3 
+SW_SHOWMAXIMIZED = 3
+SW_HIDE = 0
+SW_MINIMIZE = 6
 
 hwnd = user32.GetForegroundWindow()
 
 period = None
 start = None
+parserPeriod = re.compile(r'(\d+)(\w?)')
 if __name__ == '__main__':
 
     while True:
         if not start:
-            period = int(
-                input(f'[{datetime.datetime.now()}] Enter work period in seconds ->'))
+            unparsedPeriod = input(
+                f'[{datetime.datetime.now()}] Enter work period; "m" and "h" suffixes supported ->')
+            match = parserPeriod.search(unparsedPeriod)
+            if not match:
+                print('Input is not valid!')
+                continue
+            value = int(match.group(1))
+            suffix = match.group(2)
+            coef = 1
+            if suffix == 'm':
+                coef = 60
+            elif suffix == 'h':
+                coef = 3600
+            period = value*coef
             start = datetime.datetime.now()
+            user32.ShowWindow(hwnd, SW_MINIMIZE)
         elif (datetime.datetime.now() - start).total_seconds() > period:
             start = None
             winsound.PlaySound('brokenGlass.wav',
