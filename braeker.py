ac343c6f8bee485861f2fbb8527e27801d4212bd
OMOH98 <gleb.kosiachenko@logika.com.ua>
2020-06-25 09:33:49 +0300

    Setting focus

--- braeker.py
+++ braeker.py
@@ -9,6 +9,10 @@ user32 = ctypes.windll.user32
 SW_SHOWMAXIMIZED = 3
 SW_HIDE = 0
 SW_MINIMIZE = 6
+keybd_event = ctypes.windll.user32.keybd_event
+alt_key = 0x12
+extended_key = 0x0001
+key_up = 0x0002
 
 hwnd = user32.GetForegroundWindow()
 
@@ -34,11 +38,14 @@ if __name__ == '__main__':
                 coef = 3600
             period = value*coef
             start = datetime.datetime.now()
-            user32.ShowWindow(hwnd, SW_MINIMIZE)
+            user32.ShowWindow(hwnd, SW_HIDE)
         elif (datetime.datetime.now() - start).total_seconds() > period:
             start = None
             winsound.PlaySound('brokenGlass.wav',
                                winsound.SND_FILENAME | winsound.SND_ASYNC)
             user32.ShowWindow(hwnd, SW_SHOWMAXIMIZED)
+            keybd_event(alt_key, 0, extended_key | 0, 0)
+            user32.SetForegroundWindow(hwnd)
+            keybd_event(alt_key, 0, extended_key | key_up, 0)
         else:
             time.sleep(period)
