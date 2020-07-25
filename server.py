7fdd74a26672273eb069372b0941923ea34791d4
OMOH98 <gleb.kosiachenko@logika.com.ua>
2020-07-25 13:36:13 +0300

    add timeout and sleep to loop

--- server.py
+++ server.py
@@ -1,16 +1,23 @@
 import socket
+import time
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 ENCODING = 'utf-8'
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
+    socket.setdefaulttimeout(0.5)
     s.listen(1)
     conn, addr = s.accept()
+    data = None
     with conn:
         print('Connected by', addr)
         while True:
-            data = conn.recv(1024)
+            time.sleep(2)
+            try:
+                data = conn.recv(1024)
+            except socket.timeout:
+                continue
             if not data:
                 break
             print(data.decode(ENCODING), sep='', end='')
