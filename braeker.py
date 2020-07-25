8898e0ec4292931e541a0ac8adc63ee36a0300e6
OMOH98 <gleb.kosiachenko@logika.com.ua>
2020-07-25 15:09:22 +0300

    Add single instance functionality Running another instance brings background to front

--- braeker.py
+++ braeker.py
@@ -3,6 +3,8 @@ import time
 import datetime
 import re
 import ctypes
+import socket
+import threading
 from ctypes import wintypes
 
 user32 = ctypes.windll.user32
@@ -14,12 +16,46 @@ alt_key = 0x12
 extended_key = 0x0001
 key_up = 0x0002
 
+HOST = '127.0.0.1'
+PORT = 51476
+REFRESH_PERIOD = 1
+
 hwnd = user32.GetForegroundWindow()
 
 period = None
 start = None
 parserPeriod = re.compile(r'(\d+)(\w?)')
+
+showUpInterruption = False
+
+def ListenToConnection():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind((HOST, PORT))
+        except OSError:
+            s.connect((HOST, PORT))
+            print(f'Unexpected bind error. Exiting!')
+            exit()
+
+        s.listen(2)
+        while True:
+            conn, addr = s.accept()
+            conn.close()
+            global showUpInterruption
+            showUpInterruption = True
+
+
 if __name__ == '__main__':
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind((HOST, PORT))
+        except OSError as err:
+            s.connect((HOST, PORT))
+            exit()
+
+    time.sleep(0.5)
+    listenerThread = threading.Thread(target=ListenToConnection, daemon=True)
+    listenerThread.start()
 
     while True:
         if not start:
@@ -39,8 +75,10 @@ if __name__ == '__main__':
             period = value*coef
             start = datetime.datetime.now()
             user32.ShowWindow(hwnd, SW_HIDE)
+            showUpInterruption = False
         elif (datetime.datetime.now() - start).total_seconds() > period:
             start = None
+            showUpInterruption = False
             winsound.PlaySound('brokenGlass.wav',
                                winsound.SND_FILENAME | winsound.SND_ASYNC)
             user32.ShowWindow(hwnd, SW_SHOWMAXIMIZED)
@@ -48,4 +86,8 @@ if __name__ == '__main__':
             user32.SetForegroundWindow(hwnd)
             keybd_event(alt_key, 0, extended_key | key_up, 0)
         else:
-            time.sleep(period)
+            time.sleep(REFRESH_PERIOD)
+            if showUpInterruption:
+                start = None
+                user32.ShowWindow(hwnd, SW_SHOWMAXIMIZED)
+                showUpInterruption = False
